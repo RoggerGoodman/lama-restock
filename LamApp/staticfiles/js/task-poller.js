6e34cc75114cce44e795c621c86eb5d3976e2409
RoggerGoodman <ruggero795@gmail.com>
2026-05-02 17:16:45 +0200

    pluralize italian fix, infinite loadings fix

--- LamApp/staticfiles/js/task-poller.js
+++ LamApp/staticfiles/js/task-poller.js
@@ -6,38 +6,38 @@ class TaskPoller {
         this.maxPolls = options.maxPolls || 360;
         this.pollCount = 0;
         this.intervalId = null;
-        
+
         this.onProgress = options.onProgress || this.defaultOnProgress;
         this.onSuccess = options.onSuccess || this.defaultOnSuccess;
         this.onError = options.onError || this.defaultOnError;
         this.onTimeout = options.onTimeout || this.defaultOnTimeout;
     }
-    
+
     start() {
         this.intervalId = setInterval(() => this.poll(), this.pollInterval);
         this.poll(); // Check immediately
     }
-    
+
     stop() {
         if (this.intervalId) {
             clearInterval(this.intervalId);
             this.intervalId = null;
         }
     }
-    
+
     async poll() {
         this.pollCount++;
-        
+
         if (this.pollCount > this.maxPolls) {
             this.stop();
             this.onTimeout();
             return;
         }
-        
+
         try {
             const response = await fetch(`/tasks/${this.taskId}/status/`);
             const data = await response.json();
-            
+
             if (data.ready) {
                 this.stop();
                 if (data.success) {
@@ -53,24 +53,24 @@ class TaskPoller {
             // Don't stop polling on network errors
         }
     }
-    
+
     // Default handlers (can be overridden)
     defaultOnProgress(data) {
         console.log('Progress:', data.progress || 0);
     }
-    
+
     defaultOnSuccess(data) {
         console.log('Success:', data);
         if (data.redirect_url) {
             window.location.href = data.redirect_url;
         }
     }
-    
+
     defaultOnError(error) {
         console.error('Task failed:', error);
         alert('Task failed: ' + error);
     }
-    
+
     defaultOnTimeout() {
         console.error('Task timed out');
         alert('Task timed out. Please refresh to check status.');
